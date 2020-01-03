8af46335e47b72dd3eb966689072a86c372800a3
chiarvlad <58225730+chiarvlad@users.noreply.github.com>
2020-01-03 20:08:01 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        class Person
+        {
+            private string firstName;
+            private string lastName;
+            public string test = "public variable";
+
+            public Person(string First, string Last)
+            {
+                Console.WriteLine("A new person is created");
+                firstName = First;
+                lastName = Last;
+            }
+
+            public string getFullName() => (firstName + " " + lastName);
+        }
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello!");
+
+           Person Vlad = new Person("Vlad", "Anastasiei");
+           Console.WriteLine($"The name of the new person is {Vlad.getFullName()}");
+           Console.WriteLine(Vlad.test);
+        
+           
+        }
+    }
+}

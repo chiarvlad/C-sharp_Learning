8ec13f05f1759e3aa7fa14456d23385f453efda3
chiarvlad <58225730+chiarvlad@users.noreply.github.com>
2020-01-03 20:27:19 +0200

    added a new line

--- Program.cs
+++ Program.cs
@@ -27,6 +27,8 @@ namespace myApp
            Console.WriteLine($"The name of the new person is {Vlad.getFullName()}");
            Console.WriteLine(Vlad.test);
         
+           Console.WriteLine("second commit test");
+
            
         }
     }
